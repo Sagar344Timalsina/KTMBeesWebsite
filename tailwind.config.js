bd86c29d75b4a0af0016adecb0171c0dffc4abbf
Pratikshya Shrestha <shresthapratikshya422@gmail.com>
2023-04-13 08:38:35 +0545

    About us done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'gray': "#64748B",
+      'white': '#ffffff',
+      'black': '#0F172A',
+      'dark-gray': '#475569',
+      'yellow': '#F0B62F',
+      'primary': '#1234ff'
+    },
+    letterSpacing: {
+      widest: '0.75px'
+    },
+    fontFamily: {
+      'sans': ['Helvetica']
+    },
+    fontSize: {
+      '2xl': '1.5rem',
+      '3xl': '2 rem'
+    },
+    spacing: {
+      '8': '8px'
+    }
   },
   plugins: [],
 }
