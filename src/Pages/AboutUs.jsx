8daedcf53c890ce03c3b45c506a25486ce33f230
Pratikshya Shrestha <shresthapratikshya422@gmail.com>
2023-04-13 13:13:34 +0545

    AboutUs Fixes

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -17,33 +17,33 @@ const AboutUs = () => {
   ]
   return (
     <>
-    <Navbar/>
-    <div className='bg-[#f1f5f9]'>
-      <Flex className=' flex-col h-40 bg-[white] justify-center items-center px-[1px] py-[30px] gap-[8px]'>
-        <Text className='text-[#64748B] font-[Helvetica] font-bold tracking-[0.75px] text-[1.5rem] leading-7 w-[202px] h-[28px] order-0'>About Company</Text>
-        <Text className='w-[492px] h-[37px] font-bold text-[32px] leading-9 text-center tracking-[0.75px] text-black font-[Helvetica] order-1'>Technology that works for you</Text>
-        <Text className='w-[175px] h-[21px] font-[Helvetica] font-normal text-lg leading-5 text-center tracking-[0.75px] text-[#475569] order-2 '>Don't just dream, do</Text>
-      </Flex>
-      <Tabs defaultValue="Our Story" className='mx-auto h-[427px]'>
-        <Tabs.List className='mt-[51px] justify-center w-[40%] mx-auto font-bold ' >
-          {tabValues.map((tabValue) => (
-            <Tabs.Tab key={tabValue.id} value={tabValue.name} color="yellow" className='focus:text-[#F0B62F] p-[20px] w-auto mr-[45px] h-[23px] font-[Helvetica]  text-[20px] leading-[23px] tracking-[0.75px] '>{tabValue.name}</Tabs.Tab>
+      <Navbar />
+      <div className='bg-light_gray'>
+        <Flex className=' flex-col h-40 bg-white justify-center items-center px-1 py-1.8rem gap-[0.5rem]'>
+          <Text className='text-gray font-sans font-bold tracking-wide text-[1.5rem] leading-7 w-[12.625rem] h-[1.75rem] order-0'>About Company</Text>
+          <Text className='w-[30.75rem] h-[2.3rem] font-bold text-[2rem] leading-9 text-center tracking-wide text-black font-sans order-1'>Technology that works for you</Text>
+          <Text className='w-[11rem] h-[1.3rem] font-sans font-normal text-lg leading-5 text-center tracking-wide text-dark_gray order-2 '>Don't just dream, do</Text>
+        </Flex>
+        <Tabs defaultValue="Our Story" className='mx-auto h-[26.7rem]'>
+          <Tabs.List className='mt-[3.2rem] justify-center w-[40%] mx-auto font-bold ' >
+            {tabValues.map((tabValue) => (
+              <Tabs.Tab key={tabValue.id} value={tabValue.name} color="yellow" className='focus:text-yellow p-[20px] w-auto mr-[45px] h-[23px] font-sans  text-[20px] leading-[23px] tracking-[0.047rem] '>{tabValue.name}</Tabs.Tab>
 
-          ))}
-        </Tabs.List>
-        {contents.map((content) => (
-          <Tabs.Panel key={content.id} value={content.name} pt='xl' className='mb-[10px] w-[60%] mx-auto' >
-            <Text className=' w-auto h-[28px] font-[Helvetica] font-bold text-[24px] leading-[28px] text-[#475569] order-0'>{content.name}</Text>
-            <Text className=' mt-[10px] w-[980px] h-[112px] font-[Helvetica] font-normal text-[18px] leading-[28px] tracking-[0.75px] text-[#475569] order-1 self-stretch'>Our combined experience of over 40 years' diligent team is more than happy to help you make your dream come true.
-              {content.describe}
-            </Text>
-          </Tabs.Panel>
-        )
+            ))}
+          </Tabs.List>
+          {contents.map((content) => (
+            <Tabs.Panel key={content.id} value={content.name} pt='xl' className='mb-3 w-[50%] mx-auto' >
+              <Text className=' w-auto h-7 font-sans font-bold text-2xl leading-7 text-dark_gray order-0'>{content.name}</Text>
+              <Text className=' mt-3 w-[61rem] h-28 font-sans font-normal text-lg leading-7 tracking-[0.047rem] text-dark_gray order-1'>Our combined experience of over 40 years' diligent team is more than happy to help you make your dream come true.
+                {content.describe}
+              </Text>
+            </Tabs.Panel>
+          )
 
-        )}
-      </Tabs>
-    </div>
-    <Footer/>
+          )}
+        </Tabs>
+      </div>
+      <Footer />
     </>
 
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'light_gray': '#f1f5f9',
+      'white': '#ffffff',
+      'yellow': '#F0B62F',
+      'gray': '#64748B',
+      'black': '#000000',
+      'dark_gray': '#475569',
+    },
+    fontFamily: {
+      'sans': ['Helvetica']
+    }
   },
   plugins: [],
 }
