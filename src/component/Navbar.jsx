1cf646b951303acb3e6cfb85cae77dc2e09f0d62
Pratikshya Shrestha <shresthapratikshya422@gmail.com>
2023-04-17 12:11:35 +0545

    [Fixes]:NavBar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import '../index.css';
-import {GiHamburgerMenu} from 'react-icons/gi'
-import {Text} from '@mantine/core';
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 const Navbar = () => {
   return (
     <nav className='flex justify-between items-center  mx-[1rem] bg-[transparent]'>
       <section className='logo'>
-        <img src="../assests/logo/bees.png" alt="Bees" className='w-44'/>
+        <img src="../assests/logo/bees.png" alt="Bees" className='w-44' />
       </section>
       <section className='hamburger__logo'>
-        <GiHamburgerMenu size={20}/>
+        <GiHamburgerMenu size={20} />
       </section>
     </nav>
-    )
+  )
 }
 
 export default Navbar
