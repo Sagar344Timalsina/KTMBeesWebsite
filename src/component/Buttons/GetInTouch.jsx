3c0fff7e4abc2c12546d7f5b6e07bf1062797d21
timalsinas768 <timalsinas768@gmail.com>
2023-04-18 16:44:02 +0545

    [Fixes]: Ready Create Components Font and Button fixes

--- src/component/Buttons/GetInTouch.jsx
+++ src/component/Buttons/GetInTouch.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const GetInTouch = () => {
   return (
-    <button className='bg-button_gray w-[12rem] h-16 rounded-full text-white text-lg font-medium'>
+    <button className='bg-button_gray w-[12rem] h-16 rounded-full text-white text-lg font-medium border border-light_gray'>
       Get in touch
     </button>
   )
