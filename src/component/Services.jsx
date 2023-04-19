1c65afab3765d859145500d4a0317203fef8e2bd
sushanmhrzn277 <sushanmhrzn277@gmail.com>
2023-04-19 12:03:27 +0545

    [Fixes]:Services

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -17,7 +17,7 @@ const Services = () => {
     return (
         <section className='bg-light_gray py-3'>
             {/* service */}
-            <div className='p-12 bg-light_gray w-9 mx-auto'>
+            <div className='p-12 bg-light_gray w-[40rem] mx-auto'>
                 <Text className='h-9 font-sans font-bold text-3xl leading-9 tracking-wider text-dark text-center'>Services</Text>
                 <Text className='font-sans font-normal text-lg text-center tracking-wider text-dark_gray self-stretch'>We're here to enable you in developing, designing, launching, and continously improving and optimizing your systems, brands, ventures, and goods.</Text>
             </div >
@@ -29,7 +29,7 @@ const Services = () => {
                         <Grid.Col span={3} className='flex flex-col items-start p-5 gap-10 h-[17.5rem] mb-6' key={service.id}>
                             <div className='items-start gap-3 h-28 z-2 w-72'>
                                 <div className='service__images absolute w-28 h-20 top-30' >
-                                    <img src={service.image} alt='icon' className='absolute left-[4.33%] right-[8.33%] top-[35%] bottom-1/4'></img>
+                                    <img src={service.image} alt='icon' className='absolute left-[4.33%] right-[8.33%] top-[35%] bottom-[25%]'></img>
                                 </div>
                                 <div className='mt-[140px] m-3'>
                                     <Text className='font-sans font-bold text-xl leading-6 tracking-wider text-dark_gray mb-3'>{service.name}</Text>
