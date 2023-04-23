2f083d1867de34cf8cae6567ee375ac67cb1d9d1
sushanmhrzn277 <sushanmhrzn277@gmail.com>
2023-04-23 18:36:39 +0545

    [Featues]:Image uploaded

--- src/component/Admin/AdminHero.jsx
+++ src/component/Admin/AdminHero.jsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import { Text, Image, SimpleGrid,TextInput,Button} from '@mantine/core';
 import {useForm,Controller} from "react-hook-form";
 import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone';
+import { createHeroSection } from '../utils/Create';
 const Hero = () => {
     const [files, setFiles] = useState([]);
     
@@ -19,27 +20,25 @@ const Hero = () => {
       });
     const {handleSubmit,control,formState:{errors}}=useForm({
         defaultValues:{
-            title1:"",
-            title2:"",
+            heading:"",
+            subheading:"",
             bgImage:"",
         }
     })
-    const onSubmit = ()=>{
-        
-    }
-    const onError = ()=>{
-        
+    const onSubmit = (data) => {
+        console.log("hello");
+        createHeroSection(files,data);
     }
   return (
     <main className='flex items-center justify-center flex-col' >
     <section className='text-4xl my-2 font-sans font-bold'>Hero Section</section>
     <section className='bg-light_gray w-[60%] shadow-2xl'>
-        <form onSubmit={handleSubmit(onSubmit, onError)} className='px-5 py-7 border-0 '>
+        <form onSubmit={handleSubmit(onSubmit)} className='px-5 py-7 border-0 '>
             <div className='flex flex-col justify-center'>
                 <div className='mb-5'>
                     <Controller
                         control={control}
-                        name='title1'
+                        name='heading'
                         rules={{
                             required: "Please fill up title"
                         }}
@@ -52,14 +51,14 @@ const Hero = () => {
                 <div className='mb-5'>
                     <Controller
                         control={control}
-                        name='title2'
+                        name='subheading'
                         rules={{
                             required: "Please fill up title"
                         }}
                         render={({ field }) => <TextInput control={control} {...field} placeholder='Title-2' size='lg' />}
                     >
                     </Controller>
-                    <p className='text-[red] px-3 font-semibold '>{errors.title2?.message}</p>
+                    <p className='text-[red] px-3 font-semibold '>{errors.subheading?.message}</p>
                 </div>
 
                 <div className='mb-2 font-light text-gray'>Background Image
@@ -74,7 +73,7 @@ const Hero = () => {
                         }
                         render={({ field }) =><>
                         <div>
-                            <Dropzone accept={IMAGE_MIME_TYPE} onDrop={setFiles}>
+                            <Dropzone {...field} accept={IMAGE_MIME_TYPE} onDrop={setFiles}>
                                 <Text align="center">Drop images here</Text>
                             </Dropzone>
 
