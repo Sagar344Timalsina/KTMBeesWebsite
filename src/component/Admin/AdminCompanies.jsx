a133294d9f5274045c762646736763537dfd86c1
sushanmhrzn277 <sushanmhrzn277@gmail.com>
2023-04-24 19:28:14 +0545

    [Features]:Image retrived from db

--- src/component/Admin/AdminCompanies.jsx
+++ src/component/Admin/AdminCompanies.jsx
@@ -1,12 +1,15 @@
-import React,{useState} from 'react';
-import { Text, Image, SimpleGrid,Button} from '@mantine/core';
+import React,{useState , useEffect} from 'react';
+import { Text, Image, SimpleGrid,Button ,Table} from '@mantine/core';
 import {useForm,Controller} from "react-hook-form";
 import { Dropzone, IMAGE_MIME_TYPE } from '@mantine/dropzone';
 import { createCompanies } from '../utils/Create';
+import { displayComapnies } from '../utils/Display';
+import {FaEdit} from 'react-icons/fa'; 
+import {MdOutlineDeleteOutline} from 'react-icons/md'; 
 
 const AdminCompanies = () => {
     const [files, setFiles] = useState([]);
-    
+    const [display,setDisplay] = useState([]);
     const previews = files.map((file, index) => {
         const imageUrl = URL.createObjectURL(file);
         
@@ -28,6 +31,17 @@ const AdminCompanies = () => {
     })
     const onSubmit = ()=>{
         createCompanies(files);
+        setFiles([]);
+    }
+    useEffect(() => {
+        const display=displayComapnies();
+        display.then((data)=>{
+            setDisplay(data);
+        }).catch((err)=>console.error(err))
+    }, [display]);
+    
+    const removeImage = () => {
+        setFiles([]);
     }
   return (
     <main className='flex items-center justify-center flex-col' >
@@ -53,6 +67,7 @@ const AdminCompanies = () => {
                                 mt={previews.length > 0 ? 'xl' : 0}
                             >
                                 {previews}
+                                {files.length > 0 && <Button className='text-black' onClick={removeImage}>Remove</Button>}
                             </SimpleGrid>
                             </div>
                         </>
@@ -64,6 +79,26 @@ const AdminCompanies = () => {
                 <Button type='submit' color='yellow' className='bg-yellow font-sans w-[20%] rounded-3xl'>CREATE</Button>
         </form>
     </section>
+    <section className='bg-light_gray w-[60%] shadow-2xl m-9'>
+                <Table horizontalSpacing="xl" verticalSpacing="lg" className='p-7' striped withColumnBorders>
+                    <thead>
+                        <tr >
+                            <th>Image</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    {display && display.map((data) => (
+                        <tbody key={data.id}>
+                            <tr>
+                                <td><img src={data.image} alt="Abc" className='w-24'/></td>
+                                <td><Button className='bg-yellow font-sans text-black'><FaEdit />Update</Button></td>
+                                <td><Button className='bg-red font-sans text-black'><MdOutlineDeleteOutline />Delete</Button></td>
+                            </tr>
+                        </tbody>
+                    ))}
+                </Table>
+            </section>
 </ main >
   )
 }
---
+++ src/component/utils/Display.js
@@ -0,0 +1,15 @@
+import {db} from '../../config/firebase';
+import {collection, getDocs} from 'firebase/firestore';
+
+export const displayComapnies = async () =>{
+    const aboutCollection = collection(db, "companies");
+        try {
+            const aboutData = await getDocs(aboutCollection);
+            const filterdData = aboutData.docs.map((doc) => ({
+                ...doc.data(), id: doc.id
+            }));
+            return(filterdData);
+        } catch (error) {
+            console.error(error);
+        }
+}
