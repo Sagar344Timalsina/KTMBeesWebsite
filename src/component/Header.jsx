817b5677ceb2ed11b66197c3ce6b73bdd7f09f9a
Pratikshya Shrestha <shresthapratikshya422@gmail.com>
2023-04-27 15:29:56 +0545

    [Features/ServiceFetch]:Dynamic display changes in services section

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,31 +4,30 @@ import Bees from '../assets/images/hero.png';
 import DisplayData from '../utils/DisplayData';
 
 const Header = () => {
-    const [display,setDisplay]=useState([]);
-    const  fetchData=async()=>{
-        const datas=await DisplayData("herosection");
-        console.log(datas);
+    const [display, setDisplay] = useState([]);
+    const fetchData = async () => {
+        const datas = await DisplayData("herosection");
         setDisplay(datas);
     }
     useEffect(() => {
         fetchData();
-    },[]);
-    return( 
+    }, []);
+    return (
         <header>
             {
-                display.map((data)=>(
-                    <div className='flex  items-center h-[95vh]  bg-contain bg-no-repeat' style={{backgroundImage:`url(${data.image})`}}>
-                    <section className='content ml-52 mb-16'>
-                        <span className='title'>
-                            <h1 className='text-6xl font-bold mb-4'>{data.heading}</h1>
-                            <h6 className='mb-10 text-2xl text-[#475569]'>{data.subheading}</h6>
-                        </span>
-                    <Button color='dark' className=' bg-black rounded-full h-16 w-48 text-base'>Get in touch</Button>
-                    </section>
-                </div>
+                display.map((data) => (
+                    <div className='flex  items-center h-[95vh]  bg-contain bg-no-repeat' key={data.id} style={{ backgroundImage: `url(${data.image})` }}>
+                        <section className='content ml-52 mb-16'>
+                            <span className='title'>
+                                <h1 className='text-6xl font-bold mb-4'>{data.heading}</h1>
+                                <h6 className='mb-10 text-2xl text-[#475569]'>{data.subheading}</h6>
+                            </span>
+                            <Button color='dark' className=' bg-black rounded-full h-16 w-48 text-base'>Get in touch</Button>
+                        </section>
+                    </div>
                 ))
             }
-        </header> 
+        </header>
     )
 }
 
--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -1,30 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, Grid } from '@mantine/core'
-import Vector from '../assets/images/Vector.png';
-import Pen from '../assets/images/Pen.png';
-import Vector3 from '../assets/images/Vector3.png';
-import Cart from '../assets/images/Cart.png';
+import DisplayData from '../utils/DisplayData';
 
 const Services = () => {
-    const services = [
-        { id: 1, name: "Mobile App Development", describe: 'Explore the avenue of improvement on the digital frontire', image: Vector },
-        { id: 2, name: "Digital Product Design", describe: 'Explore the avenue of improvement on the digital frontire', image: Pen },
-        { id: 3, name: "Web Development", describe: 'Explore the avenue of improvement on the digital frontire', image: Vector3 },
-        { id: 4, name: "Ecommerce Platform", describe: 'Explore the avenue of improvement on the digital frontire', image: Cart },
-        { id: 5, name: "Tech Consultation", describe: 'Explore the avenue of improvement on the digital frontire', image: Pen },
-        { id: 6, name: "Agency Partner Program", describe: 'Explore the avenue of improvement on the digital frontire', image: Pen },
-    ]
+
+    const [services, setServices] = useState([]);
+    const displayServices = async () => {
+        const listServices = await DisplayData("services");
+        setServices(listServices);
+    }
+    useEffect(() => {
+        displayServices();
+    }, []);
     return (
         <section className='bg-light_gray py-3'>
-            {/* service */}
             <div className='p-12 bg-light_gray w-[40rem] mx-auto'>
                 <Text className='h-9 font-sans font-bold text-3xl leading-9 tracking-wider text-dark text-center'>Services</Text>
                 <Text className='font-sans font-normal text-lg text-center tracking-wider text-dark_gray self-stretch'>We're here to enable you in developing, designing, launching, and continously improving and optimizing your systems, brands, ventures, and goods.</Text>
             </div >
 
-            {/* Grid */}
             < div className='w-[60%] mx-auto' >
-                <Grid className='mt-6 bg-light_gray flex justify-between items-center gap-1 mb-20'>
+                <Grid className='mt-6 bg-light_gray flex justify-between items-center gap-1 mb-20' >
                     {services.map((service) => (
                         <Grid.Col span={3} className='flex flex-col items-start p-5 gap-10 h-[17.5rem] mb-6' key={service.id}>
                             <div className='items-start gap-3 h-28 z-2 w-72'>
@@ -32,15 +28,14 @@ const Services = () => {
                                     <img src={service.image} alt='icon' className='absolute left-[4.33%] right-[8.33%] top-[35%] bottom-[25%]'></img>
                                 </div>
                                 <div className='mt-[140px] m-3'>
-                                    <Text className='font-sans font-bold text-xl leading-6 tracking-wider text-dark_gray mb-3'>{service.name}</Text>
-                                    <Text className=' font-sans font-normal text-lg leading-7 text-dark_gray order-1 self-stretch flex-grow-0'>{service.describe}</Text>
+                                    <Text className='font-sans font-bold text-xl leading-6 tracking-wider text-dark_gray mb-3'>{service.headingtitle}</Text>
+                                    <Text className=' font-sans font-normal text-lg leading-7 text-dark_gray order-1 self-stretch flex-grow-0'>{service.text}</Text>
                                 </div>
                             </div>
                         </Grid.Col>
                     ))}
                 </Grid>
             </div>
-
         </section >
     )
 }
