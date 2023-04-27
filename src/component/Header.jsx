eccf7cd975d2b06048d8ae207d538d09e91eed01
sushanmhrzn277 <sushanmhrzn277@gmail.com>
2023-04-27 14:54:51 +0545

    [Features]:Hero section data fetched from FireStore

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,17 +1,35 @@
 import { Button } from '@mantine/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Bees from '../assets/images/hero.png';
+import DisplayData from '../utils/DisplayData';
 
 const Header = () => {
-    <div className='flex  items-center h-[95vh]  bg-contain bg-no-repeat' style={{backgroundImage:`url(${Bees})`}}>
-        <section className='content ml-52 mb-16'>
-            <span className='title'>
-                <h1 className='text-6xl font-bold mb-4'>Experience what's next</h1>
-                <h6 className='mb-10 text-2xl text-[#475569]'>It’s all about customer Experience</h6>
-            </span>
-        <Button color='dark' className=' bg-black rounded-full h-16 w-48 text-base'>Get in touch</Button>
-        </section>
-    </div>
+    const [display,setDisplay]=useState([]);
+    const  fetchData=async()=>{
+        const datas=await DisplayData("herosection");
+        console.log(datas);
+        setDisplay(datas);
+    }
+    useEffect(() => {
+        fetchData();
+    },[]);
+    return( 
+        <header>
+            {
+                display.map((data)=>(
+                    <div className='flex  items-center h-[95vh]  bg-contain bg-no-repeat' style={{backgroundImage:`url(${data.image})`}}>
+                    <section className='content ml-52 mb-16'>
+                        <span className='title'>
+                            <h1 className='text-6xl font-bold mb-4'>{data.heading}</h1>
+                            <h6 className='mb-10 text-2xl text-[#475569]'>{data.subheading}</h6>
+                        </span>
+                    <Button color='dark' className=' bg-black rounded-full h-16 w-48 text-base'>Get in touch</Button>
+                    </section>
+                </div>
+                ))
+            }
+        </header> 
+    )
 }
 
 
