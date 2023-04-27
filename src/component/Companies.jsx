e8a8c983af3c40cee18cad1a4ca2df0bf7242e03
Pratikshya Shrestha <shresthapratikshya422@gmail.com>
2023-04-27 15:18:05 +0545

    [Features/StartupFetch]

--- src/component/Companies.jsx
+++ src/component/Companies.jsx
@@ -1,37 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, Grid } from '@mantine/core'
-import Giz from "../assets/images/Giz.png"
-import Precise from "../assets/images/Precise.png"
-import SSEE from "../assets/images/SSEE.png"
-import Collaborate from "../assets/images/Collaborate.png"
-import QMED from "../assets/images/QMED.png"
-import ABF from "../assets/images/ABF.png"
-import Dami from "../assets/images/Dami.png"
-import AWS2 from "../assets/images/AWS2.png"
-import BitSquare from "../assets/images/BitSquare.png"
-import CityTour from "../assets/images/CityTour.png"
+import DisplayData from '../utils/DisplayData'
 
 const Companies = () => {
-    const companyImages = [
-        { id: 1, image: Giz },
-        { id: 2, image: Precise },
-        { id: 3, image: SSEE },
-        { id: 4, image: Collaborate },
-        { id: 5, image: QMED },
-        { id: 6, image: ABF }
-    ]
-    const partners = [
-        { id: 1, image: Dami },
-        { id: 2, image: AWS2 },
-        { id: 3, image: BitSquare },
-        { id: 4, image: CityTour },
-    ]
+    const [company, setCompany] = useState([]);
+    const displayCompany = async () => {
+        const listCompanies = await DisplayData("companies");
+        setCompany(listCompanies);
+    }
+    const [startup, setStartup] = useState([]);
+    const displayStartup = async () => {
+        const listStartup = await DisplayData("startup");
+        setStartup(listStartup);
+    }
+    useEffect(() => {
+        displayCompany();
+        displayStartup();
+    }, []);
+
     return (
         <div className='bg-light_gray'>
             <div>
                 <Text className='flex flex-col items-center gap-2 font-sans font-bold text-3xl text-black p-12 '>Companies that trusted us</Text>
                 <Grid className='flex items-start justify-between gap-16 order-1 mx-56 mt-6 pb-12'>
-                    {companyImages.map((data) => (
+                    {company.map((data) => (
                         <Grid.Col span={3} key={data.id} className='flex justify-center'>
                             <img src={data.image} alt='Company' ></img>
                         </Grid.Col>
@@ -41,7 +33,7 @@ const Companies = () => {
             <div>
                 <Text className='flex flex-col items-center gap-2 font-sans font-bold text-3xl text-black p-12 '>Startup Partners</Text>
                 <Grid className='flex items-center justify-between mx-56 pb-12'>
-                    {partners.map((data) => (
+                    {startup.map((data) => (
                         <Grid.Col md={2} key={data.id}>
                             <img src={data.image} alt='Partners' className='flex justify-center'></img>
                         </Grid.Col>
