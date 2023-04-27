eb5f41cea55e59b46f3cb7c5a351b9c90b57c8dc
sushanmhrzn277 <sushanmhrzn277@gmail.com>
2023-04-27 14:39:33 +0545

    ABc

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,7 @@ import { Button } from '@mantine/core';
 import React from 'react';
 import Bees from '../assets/images/hero.png';
 
-const Header = () => (
+const Header = () => {
     <div className='flex  items-center h-[95vh]  bg-contain bg-no-repeat' style={{backgroundImage:`url(${Bees})`}}>
         <section className='content ml-52 mb-16'>
             <span className='title'>
@@ -12,6 +12,7 @@ const Header = () => (
         <Button color='dark' className=' bg-black rounded-full h-16 w-48 text-base'>Get in touch</Button>
         </section>
     </div>
-)
+}
+
 
 export default Header
