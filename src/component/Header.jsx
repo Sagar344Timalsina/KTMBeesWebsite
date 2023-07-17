885c667b4465b0a967e84de3e337bb23fd8b1844
sushanmhrzn277 <sushanmhrzn277@gmail.com>
2023-07-17 13:52:20 +0545

    [Fixed]:Hero Section responsive

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -17,15 +17,15 @@ const Header = () => {
       <img
         alt="Bess"
         src={topFrame}
-        className="absolute right-0 top-0 z-0 hidden sm:w-3/5   md:block"
+        className="absolute right-0 top-0 z-0 hidden sm:w-3/5  md:block"
       />
       {display.map((data) => (
         <div
-          className="flex h-[55vh] lg:h-[85vh] bg-center bg-cover overflow-hidden "
+          className="flex h-[60vh] lg:h-screen bg-center bg-cover overflow-hidden "
           key={data?.id}
           // style={{ backgroundImage: `url(${data?.image})` }}
         >
-          <section className="content mt-14 md:mt-44 mx-auto z-20">
+          <section className="content mt-28 md:mt-44 mx-auto z-20">
             <span className="title">
               <h1 className="w-4/5  sm:text-4xl xl:text-5xl 2xl:text-6xl font-bold sm:mb-4 mb-3">
                 {data?.heading}
