bc3ba72602e50e9c1d612fc4cf03f26340410723
Saish <saishmanandhar5@gmail.com>
2023-11-21 14:45:19 +0545

    new website ktmbees

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { HashLink } from "react-router-hash-link";
 import { ImLocation } from "react-icons/im";
 import { FiPhoneCall } from "react-icons/fi";
-import { AiOutlineMail } from "react-icons/ai";
+import np from "../assets/images/np.png";
+import us from "../assets/images/us.png";
+import phone from "../assets/images/phone.png";
 
 export const Footer = () => {
   const quickLinks = [
@@ -19,52 +21,82 @@ export const Footer = () => {
     { id: 4, name: "CSR", link: "/" },
   ];
   const contact = [
-    { id: 1, logo: <ImLocation />, name: "Baneshwor, Kathmandu", link: "/" },
-    { id: 2, logo: <FiPhoneCall />, name: "+977-9800022222", link: "/" },
-    { id: 3, logo: <AiOutlineMail />, name: "info@ktmbees.com", link: "/" },
+    {
+      id: 1,
+      logo: us,
+      name: "3349 Haverford Dr Canton MI 48188 Michigan, United States",
+      link: "/",
+      icon: <FiPhoneCall />,
+      phone: "+1 (910) 978-7328",
+    },
+    {
+      id: 3,
+      logo: np,
+      name: "Baneshwor, Kathmandu, Nepal",
+      link: "/",
+      icon: <FiPhoneCall />,
+      phone: "+977-9800022222",
+    },
   ];
   return (
     <div className="footer flex bg-[#111111] flex-col">
-      <section className="w-10/12 sm:mt-10 ml-10 mt-10 mb-2 text-white sm:w-3/4  sm:mb-4 sm:m-auto sm:flex justify-between">
-        <article className="quick-link flex flex-col mb-6">
-          <div className="text-sm sm:mb-4 mb-2 sm:text-2xl">Quick Links</div>
-          {quickLinks.map((data) => (
-            <HashLink
-              to={data.link}
-              className="text-xs links mb-2 sm:text-lg"
-              key={data.id}
-              smooth
-            >
-              {data.name}
-            </HashLink>
-          ))}
-        </article>
-        <article className="about flex flex-col mb-6">
-          <div className="text-sm sm:mb-4 mb-2 sm:text-2xl">About us</div>
-          {about.map((data) => (
-            <HashLink
-              to={data.link}
-              className="text-xs links mb-1 sm:mb-2 sm:text-lg"
-              key={data.id}
-            >
-              {data.name}
-            </HashLink>
-          ))}
-        </article>
-        <article className="contact flex flex-col mb-6">
-          <div className="text-sm sm:mb-4 mb-2 sm:text-2xl">Contact</div>
-          {contact.map((data) => (
-            <div key={data.id}>
+      <section className="w-[80%] sm:w-[75%] m-auto pt-14 flex justify-between items-center overflow-hiddenv ">
+        <section className="text-white grid grid-cols-1 lg:grid-cols-4   justify-center items-center">
+          <article className="quick-link flex flex-col mb-6 col-span-1">
+            <div className="text-sm sm:mb-4 mb-2 sm:text-2xl">Quick Links</div>
+            {quickLinks.map((data) => (
               <HashLink
-                className="flex items-center text-xs links mb-1 sm:mb-2 sm:text-lg"
                 to={data.link}
+                className="text-xs links mb-2 sm:text-lg"
+                key={data.id}
+                smooth
               >
-                <span className="mr-4">{data.logo}</span>
-                <h6>{data.name}</h6>
+                {data.name}
               </HashLink>
+            ))}
+          </article>
+          <article className="about flex flex-col mb-6 col-span-1">
+            <div className="text-sm sm:mb-4 mb-2 sm:text-2xl">About us</div>
+            {about.map((data) => (
+              <HashLink
+                to={data.link}
+                className="text-xs links mb-1 sm:mb-2 sm:text-lg"
+                key={data.id}
+              >
+                {data.name}
+              </HashLink>
+            ))}
+          </article>
+          <article className="contact flex flex-col mb-6 col-span-1 md:col-span-2">
+            <div className="text-sm sm:mb-4 mb-2 sm:text-2xl">Address</div>
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+              {contact?.map((data) => (
+                <div key={data.id} className="px-3 py-2 md:py-0">
+                  <HashLink
+                    className="flex items-start justify-start text-xs links flex-col mb-1 sm:mb-2 sm:text-lg"
+                    to={data?.link}
+                  >
+                    <div className="flex py-2 justify-start">
+                      <span className="mr-4 flex items-center justify-center">
+                        <img
+                          src={data.logo}
+                          height={30}
+                          width={30}
+                          alt="logo"
+                        />
+                      </span>
+                      <h6 className="w-[80%]">{data?.name}</h6>
+                    </div>
+                    <div className="flex py-2 justify-start">
+                      <span className="mr-4 flex items-center justify-center">{data?.icon}</span>
+                      <h6>{data?.phone}</h6>
+                    </div>
+                  </HashLink>
+                </div>
+              ))}
             </div>
-          ))}
-        </article>
+          </article>
+        </section>
       </section>
       <hr className="w-[80%] sm:w-[75%] m-auto mb-5 border-[1px] border-[gray]" />
       <section className="w-[80%] sm:w-[75%] m-auto mb-5 flex justify-between items-center overflow-hidden">
